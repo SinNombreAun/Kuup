408138e863051cba57bb277bb4f227a185d6fef6
Ramon Torres <ramon9401@gmail.com>
2020-10-03 21:08:21 -0500

    Se agregaron clases
    Proveedor

---
+++ Negocio.Kuup/Clases/ClsProveedores.cs
@@ -0,0 +1,170 @@
+﻿using Mod.Entity;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Migrations.Builders;
+using System.IO.Pipes;
+using System.Linq;
+using System.Security.Permissions;
+namespace Negocio.Kuup.Clases
+{
+    public class ClsProveedores : Interfaces.InterfazGen<ClsProveedores>
+    {
+        ViProveedor Proveedor = new ViProveedor();
+        public byte NumProveedor
+        {
+            get { return Proveedor.PRV_NUM_PROVEEDOR; }
+            set { Proveedor.PRV_NUM_PROVEEDOR = value;}
+        }
+        public String NombreDeProveedor
+        {
+            get { return Proveedor.PRV_NOM_PROVEEDOR; }
+            set { Proveedor.PRV_NOM_PROVEEDOR = value; }
+        }
+        public System.DateTime FechaDeRegistro
+        {
+            get { return Proveedor.PRV_FECHA_REGISTRO; }
+            set { Proveedor.PRV_FECHA_REGISTRO = value; }
+        }
+        public byte SurtidoPorCorreo
+        {
+            get { return Proveedor.PRV_CVE_SURTIDO_POR_CORREO; }
+            set { Proveedor.PRV_CVE_SURTIDO_POR_CORREO = value; }
+        }
+        public String Correo
+        {
+            get { return Proveedor.PRV_CORREO; }
+            set { Proveedor.PRV_CORREO = value; }
+        }
+        public String Asunto
+        {
+            get { return Proveedor.PRV_ASUNTO; }
+            set { Proveedor.PRV_ASUNTO = value; }
+        }
+        public String Mensaje
+        {
+            get { return Proveedor.PRV_MENSAJE; }
+            set { Proveedor.PRV_MENSAJE = value; }
+        }
+        public Byte Estatus
+        {
+            get { return Proveedor.PRV_CVE_ESTATUS; }
+            set { Proveedor.PRV_CVE_ESTATUS = value; }
+        }
+        public String TextoSurtidoPorCorreo
+        {
+            get { return Proveedor.PRV_TXT_SURTIDO_POR_CORREO; }
+            set { Proveedor.PRV_TXT_SURTIDO_POR_CORREO = value; }
+        }
+        public String TextoEstatus
+        {
+            get { return Proveedor.PRV_TXT_ESTATUS; }
+            set { Proveedor.PRV_TXT_ESTATUS = value; }
+        }
+        public bool Insert()
+        {
+            try
+            {
+                using (DBKuupEntities db = new DBKuupEntities())
+                {
+                    Proveedor Proveedor = this.ToTable();
+                    db.Proveedor.Add(Proveedor);
+                    db.SaveChanges();
+                    if ((from q in db.Proveedor where q.PRV_NUM_PROVEEDOR == Proveedor.PRV_NUM_PROVEEDOR select q).Count() != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+        public bool Delete()
+        {
+            try
+            {
+                using (DBKuupEntities db = new DBKuupEntities())
+                {
+                    db.Proveedor.Remove((from q in db.Proveedor where q.PRV_NUM_PROVEEDOR == Proveedor.PRV_NUM_PROVEEDOR select q).FirstOrDefault());
+                    db.SaveChanges();
+                    if ((from q in db.Proveedor where q.PRV_NUM_PROVEEDOR == Proveedor.PRV_NUM_PROVEEDOR select q).Count() != 0)
+                    {
+                        return false;
+                    }
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+        public bool Update()
+        {
+            throw new NotImplementedException();
+        }
+        public Proveedor ToTable()
+        {
+            Proveedor Tabla = new Proveedor();
+            Tabla.PRV_NUM_PROVEEDOR = this.NumProveedor;
+            Tabla.PRV_NOM_PROVEEDOR = this.NombreDeProveedor;
+            Tabla.PRV_FECHA_REGISTRO = this.FechaDeRegistro;
+            Tabla.PRV_CVE_SURTIDO_POR_CORREO = this.SurtidoPorCorreo;
+            Tabla.PRV_CORREO = this.Correo;
+            Tabla.PRV_ASUNTO = this.Asunto;
+            Tabla.PRV_MENSAJE = this.Mensaje;
+            Tabla.PRV_CVE_ESTATUS = this.Estatus;
+            return Tabla;
+        }
+        public static List<ClsProveedores> getList(bool EsVista = true)
+        {
+            try
+            {
+                using (DBKuupEntities db = new DBKuupEntities())
+                {
+                    if (EsVista)
+                    {
+                        return (from q in db.ViProveedor
+                                select new ClsProveedores()
+                                {
+                                    NumProveedor = q.PRV_NUM_PROVEEDOR,
+                                    NombreDeProveedor = q.PRV_NOM_PROVEEDOR,
+                                    FechaDeRegistro = q.PRV_FECHA_REGISTRO,
+                                    SurtidoPorCorreo = q.PRV_CVE_SURTIDO_POR_CORREO,
+                                    Correo = q.PRV_CORREO,
+                                    Asunto = q.PRV_ASUNTO,
+                                    Mensaje = q.PRV_MENSAJE,
+                                    Estatus = q.PRV_CVE_ESTATUS,
+                                    TextoSurtidoPorCorreo = q.PRV_TXT_SURTIDO_POR_CORREO,
+                                    TextoEstatus = q.PRV_TXT_ESTATUS
+                                }).ToList();
+                    }
+                    else
+                    {
+                        return (from q in db.Proveedor
+                                select new ClsProveedores()
+                                {
+                                    NumProveedor = q.PRV_NUM_PROVEEDOR,
+                                    NombreDeProveedor = q.PRV_NOM_PROVEEDOR,
+                                    FechaDeRegistro = q.PRV_FECHA_REGISTRO,
+                                    SurtidoPorCorreo = q.PRV_CVE_SURTIDO_POR_CORREO,
+                                    Correo = q.PRV_CORREO,
+                                    Asunto = q.PRV_ASUNTO,
+                                    Mensaje = q.PRV_MENSAJE,
+                                    Estatus = q.PRV_CVE_ESTATUS
+                                }).ToList();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            return new List<ClsProveedores>();
+        }
+    }
+}
---
+++ Presentacion.Kuup/Models/ProvvedorModel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.EnterpriseServices.Internal;
+using System.Runtime.InteropServices;
+
+namespace Presentacion.Kuup.Models
+{
+    public class ProveedorModel : Negocio.Kuup.Clases.ClsProveedores
+    {
+        public ProveedorModel() { }
+        public ProveedorModel(Negocio.Kuup.Clases.ClsProveedores proveedores)
+        {
+            this.NumProveedor = proveedores.NumProveedor;
+            this.NombreDeProveedor = proveedores.NombreDeProveedor;
+            this.FechaDeRegistro = proveedores.FechaDeRegistro;
+            this.SurtidoPorCorreo = proveedores.SurtidoPorCorreo;
+            this.Correo = proveedores.Correo;
+            this.Asunto = proveedores.Asunto;
+            this.Mensaje = proveedores.Mensaje;
+            this.Estatus = proveedores.Estatus;
+            this.TextoSurtidoPorCorreo = proveedores.TextoSurtidoPorCorreo;
+            this.TextoEstatus = proveedores.TextoEstatus;
+        }
+        [Required]
+        [Display(Name = "Número de Proveedor")]
+        public Byte fNumProveedor
+        {
+            get { return this.NumProveedor; }
+            set { this.NumProveedor = value; }
+        }
+        [Required]
+        [Display(Name = "Nombre del Proveedor")]
+        public String fNombreDeProveedor
+        {
+            get { return this.NombreDeProveedor; }
+            set { this.NombreDeProveedor = value; }
+        }
+        [Required]
+        [Display(Name = "Fecha de Registro")]
+        public System.DateTime fFechaDeRegistro
+        {
+            get { return this.FechaDeRegistro; }
+            set { this.FechaDeRegistro = value; }
+        }
+        [Required]
+        [Display(Name = "Surtido Por Correo")]
+        public Byte fSurtidoPorCorreo
+        {
+            get { return this.SurtidoPorCorreo; }
+            set { this.SurtidoPorCorreo = value; }
+        }
+        [Required]
+        [Display(Name = "Correo")]
+        public String fCorreo
+        {
+            get { return this.Correo; }
+            set { this.Correo = value; }
+        }
+        [Required]
+        [Display(Name = "Asunto")]
+        public String fAsunto
+        {
+            get { return this.Asunto; }
+            set { this.Asunto = value; }
+        }
+        [Required]
+        [Display(Name = "Mensaje")]
+        public String fMensaje
+        {
+            get { return this.Mensaje; }
+            set { this.Mensaje = value; }
+        }
+        [Required]
+        [Display(Name = "Estatus")]
+        public Byte fEstatus
+        {
+            get { return this.Estatus; }
+            set { this.Estatus = value; }
+        }
+        [Required]
+        [Display(Name = "Surtido Por Correo")]
+        public String fTextoSurtidoPorCorreo
+        {
+            get { return this.TextoSurtidoPorCorreo; }
+            set { this.TextoSurtidoPorCorreo = value; }
+        }
+        [Required]
+        [Display(Name = "Estatus")]
+        public String fTextoEstatus
+        {
+            get { return this.TextoEstatus; }
+            set { this.TextoEstatus = value; }
+        }
+    }
+}
