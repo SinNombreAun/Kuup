6199043d39f177863ae269a2800c31164698ef1d
FrEaK <sercal0121@gmail.com>
2020-12-20 22:36:06 -0600

    Versión Kuup 1.0.1-Stable

--- Presentacion.Kuup/Nucleo/Clases/ClsVersion.cs
+++ Presentacion.Kuup/Nucleo/Clases/ClsVersion.cs
@@ -19,7 +19,7 @@ namespace Presentacion.Kuup.Nucleo.Clases
         {
             VersionMayor = 1;
             VersionDeCompativilidad = 0;
-            VersionDeCompilado = 0;
+            VersionDeCompilado = 1;
             Complemento = "stable";
             FechaDeVersion = new DateTime(2020,12,20);
             NumeroDeVersion = String.Format("{0}.{1}.{2}-{3}",VersionMayor,VersionDeCompativilidad,VersionDeCompilado,Complemento);
