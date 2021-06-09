d8020c70783a4b9425585a03362b15e9bceee532
FrEaK_0121 <sercal0121@gmail.com>
2021-06-09 02:03:56 -0500

    ci: version 1.1.5

--- Presentacion.Kuup/Nucleo/Clases/ClsVersion.cs
+++ Presentacion.Kuup/Nucleo/Clases/ClsVersion.cs
@@ -19,9 +19,9 @@ namespace Presentacion.Kuup.Nucleo.Clases
         {
             VersionMayor = 1;
             VersionDeCompativilidad = 1;
-            VersionDeCompilado = 4;
+            VersionDeCompilado = 5;
             Complemento = "stable";
-            FechaDeVersion = new DateTime(2021,5,23);
+            FechaDeVersion = new DateTime(2021,6,9);
             NumeroDeVersion = String.Format("{0}.{1}.{2}-{3}",VersionMayor,VersionDeCompativilidad,VersionDeCompilado,Complemento);
             NumeroDeVersionCorta = String.Format("{0}.{1}.{2}", VersionMayor, VersionDeCompativilidad, VersionDeCompilado);
             TextoDeVersion = String.Format("Versión {0}", NumeroDeVersion);
